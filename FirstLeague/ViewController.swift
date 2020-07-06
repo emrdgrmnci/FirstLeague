45c95c2ef18588c630b710bf73ea474b3bdfaf4b
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-07-06 12:47:24 +0300

    Initial Commit

---
+++ FirstLeague/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstLeague
+//
+//  Created by Ali Emre Değirmenci on 6.07.2020.
+//  Copyright © 2020 Ali Emre Degirmenci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
