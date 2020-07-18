deca409d83d1d584d189c6d3ca1402d360477e72
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-07-18 12:33:09 +0300

    Add mock test data

--- FirstLeagueTests/FirstLeagueTests.swift
+++ FirstLeagueTests/FirstLeagueTests.swift
@@ -18,4 +18,20 @@ class FirstLeagueTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+
+    func testLeague() throws {
+        let bundle = Bundle(for: FirstLeagueTests.self)
+        let url = bundle.url(forResource: "team", withExtension: "json")!
+        let data = try Data(contentsOf: url)
+        let decoder = JSONDecoder()
+        let league = try decoder.decode(Main.self, from: data)
+
+        XCTAssertEqual(league.teams?.count, 18)
+        XCTAssertEqual(league.teams?.first?.rank, "1")
+        XCTAssertEqual(league.teams?.first?.team, "Hatayspor")
+        XCTAssertEqual(league.teams?.first?.play, "33")
+        XCTAssertEqual(league.teams?.first?.goalDistance, "18")
+        XCTAssertEqual(league.teams?.first?.point, "63")
+
+    }
 }
